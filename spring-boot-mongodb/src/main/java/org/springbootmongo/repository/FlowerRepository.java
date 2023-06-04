d1650ea25fa6a9b90c9c3868d169b8e9ff6f9411
Elif Gökpınar <72789565+elifgokpinar@users.noreply.github.com>
2023-06-04 22:58:31 +0300

    commit mongo db notes

---
+++ spring-boot-mongodb/src/main/java/org/springbootmongo/repository/FlowerRepository.java
@@ -0,0 +1,16 @@
+package org.springbootmongo.repository;
+
+import org.springbootmongo.entity.Flower;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface FlowerRepository  extends MongoRepository<Flower,String> {
+
+   /* @Query("{'flower': ?0}")*
+    Optional<Flower> findByFlower(String flowerName);*/
+   Optional<Flower> findByName(String name);
+
+}
